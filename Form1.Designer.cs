18fc8fa5cde46a7e5c24e9d90776e1a49e3630a5
Erkaandersson <Erik.andersson.94@hotmail.com>
2023-12-27 10:59:55 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿
+namespace Kalkylator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn7 = new Button();
+            commaBtn = new Button();
+            btn0 = new Button();
+            btn3 = new Button();
+            btn2 = new Button();
+            btn1 = new Button();
+            btn6 = new Button();
+            btn9 = new Button();
+            btn8 = new Button();
+            btn5 = new Button();
+            btn4 = new Button();
+            cBtn = new Button();
+            slashBtn = new Button();
+            subtractBtn = new Button();
+            addBtn = new Button();
+            multiBtn = new Button();
+            equalsBtn = new Button();
+            textBox1 = new TextBox();
+            textBox3 = new TextBox();
+            Uträkningar = new Button();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // btn7
+            // 
+            btn7.BackColor = SystemColors.ScrollBar;
+            btn7.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            btn7.Location = new Point(16, 63);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(59, 41);
+            btn7.TabIndex = 0;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = false;
+            btn7.Click += btn7_Click;
+            // 
+            // commaBtn
+            // 
+            commaBtn.BackColor = SystemColors.ScrollBar;
+            commaBtn.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            commaBtn.Location = new Point(81, 204);
+            commaBtn.Name = "commaBtn";
+            commaBtn.Size = new Size(59, 41);
+            commaBtn.TabIndex = 1;
+            commaBtn.Text = ",";
+            commaBtn.UseVisualStyleBackColor = false;
+            commaBtn.Click += commaBtn_Click;
+            // 
+            // btn0
+            // 
+            btn0.BackColor = SystemColors.ScrollBar;
+            btn0.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            btn0.Location = new Point(16, 204);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(59, 41);
+            btn0.TabIndex = 2;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = false;
+            btn0.Click += btn0_Click;
+            // 
+            // btn3
+            // 
+            btn3.BackColor = SystemColors.ScrollBar;
+            btn3.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            btn3.Location = new Point(146, 157);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(59, 41);
+            btn3.TabIndex = 3;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = false;
+            btn3.Click += btn3_Click;
+            // 
+            // btn2
+            // 
+            btn2.BackColor = SystemColors.ScrollBar;
+            btn2.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            btn2.Location = new Point(81, 157);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(59, 41);
+            btn2.TabIndex = 4;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = false;
+            btn2.Click += btn2_Click;
+            // 
+            // btn1
+            // 
+            btn1.BackColor = SystemColors.ScrollBar;
+            btn1.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            btn1.Location = new Point(16, 157);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(59, 41);
+            btn1.TabIndex = 5;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = false;
+            btn1.Click += btn1_Click;
+            // 
+            // btn6
+            // 
+            btn6.BackColor = SystemColors.ScrollBar;
+            btn6.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            btn6.Location = new Point(146, 110);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(59, 41);
+            btn6.TabIndex = 6;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = false;
+            btn6.Click += btn6_Click;
+            // 
+            // btn9
+            // 
+            btn9.BackColor = SystemColors.ScrollBar;
+            btn9.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            btn9.Location = new Point(146, 63);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(59, 41);
+            btn9.TabIndex = 7;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = false;
+            btn9.Click += btn9_Click;
+            // 
+            // btn8
+            // 
+            btn8.BackColor = SystemColors.ScrollBar;
+            btn8.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            btn8.Location = new Point(81, 63);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(59, 41);
+            btn8.TabIndex = 8;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = false;
+            btn8.Click += btn8_Click;
+            // 
+            // btn5
+            // 
+            btn5.BackColor = SystemColors.ScrollBar;
+            btn5.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            btn5.Location = new Point(81, 110);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(59, 41);
+            btn5.TabIndex = 9;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = false;
+            btn5.Click += btn5_Click;
+            // 
+            // btn4
+            // 
+            btn4.BackColor = SystemColors.ScrollBar;
+            btn4.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            btn4.Location = new Point(16, 110);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(59, 41);
+            btn4.TabIndex = 10;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = false;
+            btn4.Click += btn4_Click;
+            // 
+            // cBtn
+            // 
+            cBtn.BackColor = SystemColors.ScrollBar;
+            cBtn.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            cBtn.Location = new Point(146, 204);
+            cBtn.Name = "cBtn";
+            cBtn.Size = new Size(59, 41);
+            cBtn.TabIndex = 11;
+            cBtn.Text = "C";
+            cBtn.UseVisualStyleBackColor = false;
+            cBtn.Click += cBtn_Click;
+            // 
+            // slashBtn
+            // 
+            slashBtn.BackColor = SystemColors.ScrollBar;
+            slashBtn.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            slashBtn.Location = new Point(226, 63);
+            slashBtn.Name = "slashBtn";
+            slashBtn.Size = new Size(59, 41);
+            slashBtn.TabIndex = 12;
+            slashBtn.Text = "/";
+            slashBtn.UseVisualStyleBackColor = false;
+            slashBtn.Click += slashBtn_Click;
+            // 
+            // subtractBtn
+            // 
+            subtractBtn.BackColor = SystemColors.ScrollBar;
+            subtractBtn.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            subtractBtn.Location = new Point(226, 204);
+            subtractBtn.Name = "subtractBtn";
+            subtractBtn.Size = new Size(59, 41);
+            subtractBtn.TabIndex = 13;
+            subtractBtn.Text = "-";
+            subtractBtn.UseVisualStyleBackColor = false;
+            subtractBtn.Click += subtractBtn_Click;
+            // 
+            // addBtn
+            // 
+            addBtn.BackColor = SystemColors.ScrollBar;
+            addBtn.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            addBtn.Location = new Point(226, 157);
+            addBtn.Name = "addBtn";
+            addBtn.Size = new Size(59, 41);
+            addBtn.TabIndex = 14;
+            addBtn.Text = "+";
+            addBtn.UseVisualStyleBackColor = false;
+            addBtn.Click += addBtn_Click;
+            // 
+            // multiBtn
+            // 
+            multiBtn.BackColor = SystemColors.ScrollBar;
+            multiBtn.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            multiBtn.Location = new Point(226, 110);
+            multiBtn.Name = "multiBtn";
+            multiBtn.Size = new Size(59, 41);
+            multiBtn.TabIndex = 15;
+            multiBtn.Text = "*";
+            multiBtn.UseVisualStyleBackColor = false;
+            multiBtn.Click += multiBtn_Click;
+            // 
+            // equalsBtn
+            // 
+            equalsBtn.BackColor = SystemColors.ScrollBar;
+            equalsBtn.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            equalsBtn.Location = new Point(16, 251);
+            equalsBtn.Name = "equalsBtn";
+            equalsBtn.Size = new Size(269, 41);
+            equalsBtn.TabIndex = 16;
+            equalsBtn.Text = "=";
+            equalsBtn.UseVisualStyleBackColor = false;
+            equalsBtn.Click += equalsBtn_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Tahoma", 14.25F);
+            textBox1.Location = new Point(16, 16);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(269, 41);
+            textBox1.TabIndex = 17;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.WindowFrame;
+            textBox3.Font = new Font("Segoe UI", 14F);
+            textBox3.ForeColor = SystemColors.ButtonFace;
+            textBox3.Location = new Point(19, 298);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.ScrollBars = ScrollBars.Vertical;
+            textBox3.Size = new Size(266, 182);
+            textBox3.TabIndex = 20;
+            textBox3.Text = "\r\n\r\n\r\n";
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // Uträkningar
+            // 
+            Uträkningar.Location = new Point(199, 486);
+            Uträkningar.Name = "Uträkningar";
+            Uträkningar.Size = new Size(86, 23);
+            Uträkningar.TabIndex = 21;
+            Uträkningar.Text = "Uträkningar\r\n";
+            Uträkningar.UseVisualStyleBackColor = true;
+            Uträkningar.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(19, 486);
+            button1.Name = "button1";
+            button1.Size = new Size(86, 23);
+            button1.TabIndex = 22;
+            button1.Text = "Clear\r\n\r\n";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.WindowFrame;
+            ClientSize = new Size(583, 524);
+            Controls.Add(button1);
+            Controls.Add(Uträkningar);
+            Controls.Add(textBox3);
+            Controls.Add(textBox1);
+            Controls.Add(equalsBtn);
+            Controls.Add(multiBtn);
+            Controls.Add(addBtn);
+            Controls.Add(subtractBtn);
+            Controls.Add(slashBtn);
+            Controls.Add(cBtn);
+            Controls.Add(btn4);
+            Controls.Add(btn5);
+            Controls.Add(btn8);
+            Controls.Add(btn9);
+            Controls.Add(btn6);
+            Controls.Add(btn1);
+            Controls.Add(btn2);
+            Controls.Add(btn3);
+            Controls.Add(btn0);
+            Controls.Add(commaBtn);
+            Controls.Add(btn7);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private Button btn7;
+        private Button commaBtn;
+        private Button btn0;
+        private Button btn3;
+        private Button btn2;
+        private Button btn1;
+        private Button btn6;
+        private Button btn9;
+        private Button btn8;
+        private Button btn5;
+        private Button btn4;
+        private Button cBtn;
+        private Button slashBtn;
+        private Button subtractBtn;
+        private Button addBtn;
+        private Button multiBtn;
+        private Button equalsBtn;
+        private TextBox textBox1;
+        private TextBox textBox3;
+        private Button Uträkningar;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using Microsoft.VisualBasic.Devices;
+using System.Data;
+
+namespace Kalkylator
+{
+
+    public partial class Form1 : Form
+    {
+        //Skapar variabler float för numbers och answers som kan ta flyttal och göra uträkningar med.
+        //Skapar en char variabel för att kunna ta signs, som ska användas till operatorer i switch-sats till uträkningar.
+        float number1, number2, answer;
+        char sign = ' ';
+        // En lista för att spara historik för räkningar
+        //Använder lista för den är dynamisk, där storleken kan ändras efter önskad input. Arrayer kräver en "storlek" 
+        //när man deklarerar den, vilket inte skulle funka i detta fallet.
+        List<string> calculations = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Välkomnande meddelande
+            textBox3.Text = "Välkommen till kalkylatorn!" + Environment.NewLine + "Använd clear efter varje uträkning.";
+        }
+        //Användaren matar in matematiska operationer.
+        //Deklarerar operatorer till variabel sign för att använda i switch-sats. Sätter värde på number1 
+        //beroende på vilken siffra som är tryckt och parsar till en float.
+        private void subtractBtn_Click(object sender, EventArgs e)
+        {
+            sign = '-';
+            number1 = float.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            sign = '+';
+            number1 = float.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void multiBtn_Click(object sender, EventArgs e)
+        {
+            sign = '*';
+            number1 = float.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void slashBtn_Click(object sender, EventArgs e)
+        {
+            sign = '/';
+            number1 = float.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+        private void equalsBtn_Click(object sender, EventArgs e)
+        {
+            compute(sign);
+        }
+        // Lägga resultat till listan
+        //funktion med switch-sats för att göra uträkningar beroende på vilken operator som är tryckt.
+        //Och addera uträkningar till listan så som den ska skrivas ut till textbox3 i senare skede.
+        public void compute(char sign)
+        {
+            number2 = float.Parse(textBox1.Text);
+            switch (sign)
+            {
+                case '-':
+                    answer = number1 - number2;
+                    textBox1.Text = answer.ToString();
+                    calculations.Add($"{number1} {sign} {number2} = {answer}");
+                    break;
+
+                case '+':
+                    answer = number1 + number2;
+                    textBox1.Text = answer.ToString();
+                    calculations.Add($"{number1} {sign} {number2} = {answer}");
+                    break;
+
+                case '*':
+                    answer = number1 * number2;
+                    textBox1.Text = answer.ToString();
+                    calculations.Add($"{number1} {sign} {number2} = {answer}");
+                    break;
+
+                case '/':
+                    // Ifall användaren skulle dela med 0 visa Ogiltig inmatning!
+                    if (number2 == 0)
+                    {
+                        textBox1.Text = "Cannot divide by zero" + Environment.NewLine;
+                        break;
+                    }
+                    else
+                    {
+                        answer = number1 / number2;
+                        textBox1.Text = answer.ToString();
+                        calculations.Add($"{number1} {sign} {number2} = {answer}");
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //En kontrollstruktur för att inte kunna skriva bokstäver i textboxen för uträknmingar.
+            string text = textBox1.Text;    
+            foreach (char c in text)
+            {
+                if (!Char.IsDigit(c))
+                {
+                    textBox3.Text = "Endast siffror är tillåtna!";
+      
+                    textBox1.Clear();
+                }
+            }
+        }
+        // Användaren matar in tal
+        //Knappar för att skriva ut siffran som är tryckt i textbox1.
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+        }
+
+        private void commaBtn_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ",";
+        }
+        //För att rensa textbox 1, och göra en ny uträkning.
+        private void cBtn_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "C";
+            textBox1.Clear();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        // Fråga användaren om den vill visa tidigare resultat.
+        // Visa tidigare resultat, När man klickar på knappen uträkningar så används en foreach-loop för att skriva ut
+        //listan och skriva ut varje på en ny rad.
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+
+            foreach (string s in calculations)
+            {
+                textBox3.Text += s + Environment.NewLine;
+            }
+        }
+        //En funktion för att rensa textbox3 rutan med uträkningar.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+        }
+    }
+}
